ef6955d7e908715f62d6f5cc988fa7e025e2b8c4
Jennifer Gifford <jgifford82@gmail.com>
2022-05-29 09:49:57 -0600

    commit tree 308e4d92f4b31a0d9d8c89b6e7903ce508bbd468

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution here
+// function findMatching(drivers, string) {
+//     const namesMatch = drivers.filter(string => string === 'Bobby')
+//       return namesMatch
+//     }
+
+// function findMatching(drivers, string) {
+//     const namesMatch = drivers.filter(names => names == 'string')
+//       return namesMatch
+//     }
+
+// function findMatching(drivers, string) {
+//     const namesMatch = drivers.filter(Element => Element == string)
+//       return namesMatch
+//     }
+
+// function findMatching(drivers, string) {
+//     const namesMatch = drivers.filter(Element => Element == string.toUppercase || string.toLowercase)
+//       return namesMatch
+//     }
+
+// function findMatching(drivers, string) {
+//     const namesMatch = drivers.filter(Element => Element.toLowercase == string.toLowercase)
+//       return namesMatch
+//     }
+
+// function fuzzyMatch(drivers, string) {
+//     const result = drivers.filter(Element => Element.startsWith() === string.startsWith())
+//     return result
+//       }
+
+function findMatching(drivers, string) {
+    const namesMatch = drivers.filter(Element => Element.toLowerCase() === string.toLowerCase())
+      return namesMatch
+    }
+
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(Element => Element.startsWith(string))
+    return result
+    }
+      
+
+
+function matchName(drivers, string) {
+    const found = drivers.filter(Element => Element.name === string)
+        return found
+    }
